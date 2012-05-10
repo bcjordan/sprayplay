469ffc71877244bcea4fee48fe8b33abd3553513
Brian Jordan <bcjordan@gmail.com>
2012-05-10 16:56:09 -0400

    add incessant background music

--- main.lua
+++ main.lua
@@ -10,5 +10,10 @@ display.setStatusBar( display.HiddenStatusBar )
 -- include the Corona "storyboard" module
 local storyboard = require "storyboard"
 
+local drumHandle = audio.loadStream("drumbeat.wav")
+audio.play(drumHandle, { channel=1, loops=-1, fadein=10000 })
+
+
+
 -- load menu screen
 storyboard.gotoScene( "menu" )
